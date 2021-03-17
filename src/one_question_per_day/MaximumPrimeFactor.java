835a2213c69e3781f5d40093332c19e3835542c5
zhangxingjun <littleboyfury@gmail.com>
2021-03-17 15:18:43 +0800

    first commit

---
+++ src/one_question_per_day/MaximumPrimeFactor.java
@@ -0,0 +1,13 @@
+package one_question_per_day;
+
+/**
+ * @author littleboy
+ * @description 13195的所有质因数为5、7、13和29。
+ *              600851475143最大的质因数是多少？
+ * @date 2021/3/17 下午3:05
+ */
+public class MaximumPrimeFactor {
+    public static void main(String[] args) {
+
+    }
+}
